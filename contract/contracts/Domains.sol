897a42fdc5f59465a10668259c8a918e5f750a85
John Kim A. Querobines <querobinesj@gmail.com>
2022-05-19 20:06:52 +0800

    feat: setup domain contract

---
+++ contract/contracts/Domains.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Domains {
+    constructor() {
+        console.log("The Infinity Domains contract is ready to use.");
+    }
+}
--- contract/scripts/run.ts
+++ contract/scripts/run.ts
@@ -1,14 +1,6 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
-
-  // We get the contract to deploy
   const DomainsContract = await ethers.getContractFactory("Domains");
   const domainContract = await DomainsContract.deploy();
 
