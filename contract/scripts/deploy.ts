217ad8bf33fd579c34781e37de63a99163090563
John Kim A. Querobines <querobinesj@gmail.com>
2022-05-20 21:18:12 +0800

    feat: deploy script

--- contract/scripts/deploy.ts
+++ contract/scripts/deploy.ts
@@ -14,7 +14,26 @@ async function main() {
 
   await domainContract.deployed();
 
-  console.log("Greeter deployed to:", domainContract.address);
+  // CHANGE THIS DOMAIN TO SOMETHING ELSE! I don't want to see OpenSea full of bananas lol
+  let txn = await domainContract.register("webdev", {
+    value: ethers.utils.parseEther("0.1"),
+  });
+  await txn.wait();
+  console.log("Minted domain webdev.kk");
+
+  txn = await domainContract.setRecord("webdev", "Am I a webdev or a kk??");
+  await txn.wait();
+  console.log("Set record for webdev.kk");
+
+  const address = await domainContract.getAddress("webdev");
+  console.log("Owner of domain webdev:", address);
+
+  const balance = await ethers.provider.getBalance(domainContract.address);
+  console.log("Contract balance:", ethers.utils.formatEther(balance));
+
+  console.log("Contract address deployed to:", domainContract.address);
+
+  // * CONTRACT ADDRESS : 0x6ECBC674AD7754A472335d427c4e9d33A99F3461
 }
 
 // We recommend this pattern to be able to use async/await everywhere
