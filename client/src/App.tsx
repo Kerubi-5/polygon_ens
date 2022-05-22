8e40024cb44abf063f8ea6d1bb2981c3bbc759f6
John Kim A. Querobines <querobinesj@gmail.com>
2022-05-22 18:28:33 +0800

    feat: ethereum type

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,21 @@
+import { useEffect } from "react";
 import { Container, Header, Connect } from "./components";
 
 function App() {
+  const getEthereum = () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      return alert("Make sure you have MetaMask!");
+    }
+
+    return ethereum;
+  };
+
+  useEffect(() => {
+    getEthereum();
+  }, []);
+
   return (
     <div className="bg-slate-800 min-h-screen">
       <Container>
