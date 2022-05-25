6ce425e280e79c42df6338b8e9c103f1efd691a9
John Kim A. Querobines <querobinesj@gmail.com>
2022-05-25 21:28:46 +0800

    feat: modal form

---
+++ client/src/components/ModalForm.tsx
@@ -0,0 +1,66 @@
+import { Input, Button } from "components";
+import { useState } from "react";
+import { ethers } from "ethers";
+import { abi, CONTRACT_ADDRESS } from "const";
+
+const ModalForm = () => {
+  const [domain, setDomain] = useState<string>();
+  const [record, setRecord] = useState<string>();
+  const [loading, setLoading] = useState<boolean>();
+
+  const updateDomain = async () => {
+    if (!record || !domain) {
+      return;
+    }
+    setLoading(true);
+    console.log("Updating domain", domain, "with record", record);
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum as any);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
+
+        let tx = await contract.setRecord(domain, record);
+        await tx.wait();
+        console.log("Record set https://mumbai.polygonscan.com/tx/" + tx.hash);
+
+        // fetchMints();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <div className="absolute inset-0 m-10 p-20 h-max rounded-xl bg-slate-900">
+      <div className="grid justify-center gap-5">
+        <div>
+          <span className="text-zinc-50">Your eth name</span>
+          <Input
+            label=".kk"
+            value={domain}
+            onChange={(e) => setDomain(e.target.value)}
+          />
+        </div>
+        <div>
+          <span className="text-zinc-50">Description of your eth name</span>
+          <Input
+            label=""
+            value={record}
+            onChange={(e) => setRecord(e.target.value)}
+          />
+        </div>
+        <Button
+          variant={loading ? "disabled" : "primary"}
+          onClick={updateDomain}
+        >
+          Submit
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default ModalForm;
